f1fcc3b07a5466a216d4dfbe534db0f8dca7d5d3
titho93 <titho93.10@gmail.com>
2023-11-06 17:37:16 +0100

    [add]style

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ require_once __DIR__ . '/db/db.php';
 <head>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Css  -->
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP 1</title>
