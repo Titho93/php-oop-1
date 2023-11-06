ca3c7d06afcc64fe10f7685ad06639cfb0aceea4
titho93 <titho93.10@gmail.com>
2023-11-06 18:05:24 +0100

    [add]new movie

--- db/db.php
+++ db/db.php
@@ -3,5 +3,11 @@
 $movies = [
     new Movie ( "La minaccia fantasma", "En", "Star Wars: Episodio I - La minaccia fantasma", 1999, new Media('Episode I.jpg', "La minaccia fantasma")),
     new Movie ( "L'attacco dei cloni", "En", "Star Wars: Episodio II - L'attacco dei cloni", 2002, new Media('Episode II.jpg', "L'attacco dei cloni")),
-    new Movie ( "La vendetta dei Sith", "En", "Star Wars: Episodio III - La vendetta dei Sith", 2005, new Media('Episode III.jpg', "La vendetta dei Sith")),
+    new Movie ( "La vendetta dei Sith", "En", "Star Wars: Episodio III - La vendetta dei Sith", 2005, new Media('Episode III.webp', "La vendetta dei Sith")),
+    new Movie ( "Una nuova speranza", "En", "Star Wars: Episodio IV - Una nuova speranza", 1977, new Media('Episode IV.jpg', "Una nuova speranza")),
+    new Movie ( "L'Impero colpisce ancora", "En", "Star Wars: Episodio V - L'Impero colpisce ancora", 1980, new Media('Episode V.jpg', "L'Impero colpisce ancora")),
+    new Movie ( "Il ritorno dello Jedi", "En", "Star Wars: Episodio VI - Il ritorno dello Jedi", 1983, new Media('Episode VI.jpg', "Il ritorno dello Jedi")),
+    new Movie ( "Il risveglio della Forza", "En", "Star Wars: Episodio VII - Il risveglio della Forza", 2015, new Media('Episode VII.webp', "Il risveglio della Forza")),
+    new Movie ( "Gli ultimi Jedi", "En", "Star Wars: Episodio VIII - Gli ultimi Jedi", 2017, new Media('Episode VIII.jpg', "Gli ultimi Jedi")),
+    new Movie ( "L'ascesa di Skywalker", "En", "Star Wars: Episodio IX - L'ascesa di Skywalker", 2019, new Media('Episode IX.jpg', "L'ascesa di Skywalker")),
 ];
