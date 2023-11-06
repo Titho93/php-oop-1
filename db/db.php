5db5e42f7fdf82aed97ec87ccd2f5bf75dac7369
titho93 <titho93.10@gmail.com>
2023-11-06 16:04:46 +0100

    [add]file db

---
+++ db/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$movies = [
+    new Movie ( "La minaccia fantasma", "En", "Star Wars: Episodio I - La minaccia fantasma", 1999),
+    new Movie ( "L'attacco dei cloni", "En", "Star Wars: Episodio II - L'attacco dei cloni", 2002),
+    new Movie ( "L'attacco dei cloni", "En", "Star Wars: Episodio III - La vendetta dei Sith", 2005)
+];
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once __DIR__ . '/db/db.php';
+
 class Movie {
     public $title;
     public $language;
@@ -13,12 +15,6 @@ class Movie {
         $this->year = $_year;
     }
 }
-
-$movies = [
-    new Movie ( "La minaccia fantasma", "En", "Star Wars: Episodio I - La minaccia fantasma", 1999),
-    new Movie ( "L'attacco dei cloni", "En", "Star Wars: Episodio II - L'attacco dei cloni", 2002),
-    new Movie ( "L'attacco dei cloni", "En", "Star Wars: Episodio III - La vendetta dei Sith", 2005)
-]
 
 ?>
 
