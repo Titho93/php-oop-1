19bc5fbe455f5121350fd7be096cf1cfe17878de
titho93 <titho93.10@gmail.com>
2023-11-07 18:04:02 +0100

    [edit]series

--- Model/Movie.php
+++ Model/Movie.php
@@ -4,11 +4,11 @@ class Movie extends Production{
     public $published_year;
     public $running_time;
 
-    public function __construct( string $_title, string $_language, string $_bio, float $_year, Media $_image, float $_published_year, float $_running_time){
+    public function __construct(string $_title, string $_language, string $_bio, array $_cast, Media $_image, float $_published_year, float $_running_time){
         $this->published_year = $_published_year;
         $this->running_time = $_running_time;
 
-        parent::__construct(string $_title, string $_language, string $_bio, float $_year, Media $_image);
+        parent::__construct(string $_title, string $_language, string $_bio, array $_cast, Media $_image);
     }
 
 };
--- Model/Serie.php
+++ Model/Serie.php
@@ -2,19 +2,18 @@
 
 class Serie extends Production {
     public $aired_from_year;
-    public $$aired_to_year;
+    public $aired_to_year;
     public $number_of_episodes;
     public $number_of_seasons;
 
-    public function __construct(string $_title, string $_language, string $_bio, float $_year, Media $_image){
-        $this->title = $_title;
-        $this->language = $_language;
-        $this->bio = $_bio;
-        $this->year = $_year;
-        $this->image = $_image;
+    public function __construct(string $_title, string $_language, string $_bio, array $_cast, Media $_image, float $_aired_from_year, float $_aired_to_year, float $_number_of_episodes, float $_number_of_seasons){
+        $this->aired_from_year = $_aired_from_year;
+        $this->aired_to_year = $_aired_to_year;
+        $this->number_of_episodes = $_number_of_episodes;
+        $this->number_of_seasons = $_number_of_seasons;
+
+        parent::__construct(string $_title, string $_language, string $_bio, array $_cast, Media $_image);
     }
 
-    public function setImage(Media $_image){
-        $this->image = $_image;
-    }
+
 }
